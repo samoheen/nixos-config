6254f7a9e991c43a16c1254538ee64e147a5e2cc
Sammy O'Heen <samoheen@hotmail.com>
2025-11-25 00:08:11 +0300

    Configure Hyprland

--- nixos/hosts/zenbook/configuration.nix
+++ nixos/hosts/zenbook/configuration.nix
@@ -6,11 +6,39 @@
     ../../modules
   ];
 
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  security.polkit.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    audio.enable = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
   environment.systemPackages = with pkgs; [
     git
     vim
     wget
     htop
+
+    waybar
+    rofi-wayland
+    dunst
+    kitty
+ 
+    grim
+    slurp
+    wl-clipboard
   ];
 
   system.stateVersion = stateVersion;
