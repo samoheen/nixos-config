2cd1a5981211bd49f4d3dfe909c16025a5e68897
Sammy O'Heen <samoheen@hotmail.com>
2025-11-25 00:02:16 +0300

    Configure Hyprland

--- nixos/home-manager/modules/hyprland.nix
+++ nixos/home-manager/modules/hyprland.nix
@@ -1,4 +1,28 @@
 {
-  programs.kitty.enable = true;
-  wayland.windowManager.hyprland.enable = true;
+  wayland.windowManager.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+
+    settings = {
+      monitor = ",preferred,auto,auto";
+
+      input = {
+        kb_layout = "us,ru";
+        kb_options = "grp:alt_shift_toggle";
+        follow_mouse = 1;
+        touchpad = {
+          natural_scroll = false;
+        };
+      };
+
+      "$mod" = "SUPER";
+ 
+      bind = [
+        "$mod, RETURN, exec, kitty"
+      ];
+    };
+
+    services.dunst.enable = true;
+    programs.kitty.enable = true;
+  };
 }
