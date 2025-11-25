aad7303b66d8cb0bd8bac11b8f12367d6ddfbe4a
Sammy O'Heen <samoheen@hotmail.com>
2025-11-25 20:01:57 +0300

    Configure Hyprland

--- nixos/home-manager/modules/waybar/default.nix
+++ nixos/home-manager/modules/waybar/default.nix
@@ -9,6 +9,27 @@
         modules-left = [ "hyprland/workspaces" ];
         modules-center = [ "hyprland/window" ];
         modules-right = [ "hyprland/language" "battery"  "clock" ];
+
+        "hyprland/workspaces" = {
+          disable-scroll = true;
+          show-special = true;
+          special-visible-only = true;
+          all-outputs = false;
+        };
+
+        "hyprland/language" = {
+          format-en = "[en]";
+          format-ru = "[ru]";
+          tooltip = false;
+        };
+
+        "battery" = {
+          format = "[{capacity}]"
+        };
+
+        "clock" = {
+          format = "[{:%d.%m.%Y - %H:%M}]";
+        };
       };
     };
   };
