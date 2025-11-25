3d48e583b6edadeeaebf475386adf3cb946c302d
Sammy O'Heen <samoheen@hotmail.com>
2025-11-25 14:31:54 +0300

    Configure zsh

---
+++ nixos/home-manager/modules/zsh.nix
@@ -0,0 +1,23 @@
+{ config, ... }:
+
+{
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases =
+      let
+        flakeDir = "~/.nixos-config/nixos";
+      in {
+        gs = "git status";
+        ga = "git add";
+        gc = "git commit";
+        gp = "git push";
+      };
+
+    history.size = 10000;
+    history.path = "${config.xdg.dataHome}/zsh/history";
+  };
+}
