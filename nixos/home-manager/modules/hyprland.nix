9defdf03f66ae9734063b70d11cb2d4f73ee2cfb
Sammy O'Heen <samoheen@hotmail.com>
2025-11-25 18:56:56 +0300

    Configure Hyprland

--- nixos/home-manager/modules/hyprland.nix
+++ nixos/home-manager/modules/hyprland.nix
@@ -30,9 +30,39 @@
       };
 
       bind = [
-        "SUPER, Q, killactive,"
-        "SUPER, RETURN, exec, alacritty"
+        "SUPER_SHIFT, Q, exit,"
+        "SUPER_SHIFT, C, killactive,"
+
+        "SUPER_SHIFT, RETURN, exec, alacritty"
         "SUPER_SHIFT, Z, exec, zen"
+        "SUPER_SHIFT, X, exec, ranger"
+
+        "SUPER,       LEFT, movefocus, l"
+        "SUPER,       RIGHT, movefocus, r"
+        "SUPER,       UP, movefocus, u"
+        "SUPER,       DOWN, movefocus, d"
+
+        "SUPER,       1, workspace, 1"
+        "SUPER,       2, workspace, 2"
+        "SUPER,       3, workspace, 3"
+        "SUPER,       4, workspace, 4"
+        "SUPER,       5, workspace, 5"
+        "SUPER,       6, workspace, 6"
+        "SUPER,       7, workspace, 7"
+        "SUPER,       8, workspace, 8"
+        "SUPER,       9, workspace, 9"
+        "SUPER,       0, workspace, 10"
+
+        "SUPER_SHIFT, 1, movetoworkspacesilent, 1"
+        "SUPER_SHIFT, 2, movetoworkspacesilent, 2"
+        "SUPER_SHIFT, 3, movetoworkspacesilent, 3"
+        "SUPER_SHIFT, 4, movetoworkspacesilent, 4"
+        "SUPER_SHIFT, 5, movetoworkspacesilent, 5"
+        "SUPER_SHIFT, 6, movetoworkspacesilent, 6"
+        "SUPER_SHIFT, 7, movetoworkspacesilent, 7"
+        "SUPER_SHIFT, 8, movetoworkspacesilent, 8"
+        "SUPER_SHIFT, 9, movetoworkspacesilent, 9"
+        "SUPER_SHIFT, 0, movetoworkspacesilent, 10"
       ];
     };
   };
