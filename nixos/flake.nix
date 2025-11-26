bb70100b7c2da62a2f078c058fced6fca624a4bc
Sammy O'Heen <samoheen@hotmail.com>
2025-11-26 14:34:13 +0300

    Configure Rust & Go

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -21,7 +22,7 @@
   };
 
   outputs =
-    inputs@{ nixpkgs, home-manager, ... }: let
+    inputs@{ nixpkgs, unstable, home-manager, ... }: let
       system = "x86_64-linux";
       stateVersion = "25.05";
       user = "sam";
